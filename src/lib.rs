b188585bd22494fe31466c8c033b7d7b095d8c77
Putnam <putnam3145@gmail.com>
2021-06-06 01:20:07 -0700

    A basic FDM implementation

--- src/lib.rs
+++ src/lib.rs
@@ -36,39 +36,13 @@ pub struct Weapon {
     size: f64,
     attacks: Vec<Attack>,
 }
-
-/*function attack_score(attack, weapon_mat, armor_mat, weapon, weapon_weight) {
-    let trials = 0
-    let successes = 0
-    for(let a=6;a>=0;a--) {
-        let Qa = quality_armor_mults[a]
-        for(let w=0;w<=6;w++) {
-            const Qw = quality_weapon_mults[w]
-            for(let _=0; _ < 2; _++) {
-                const size = 60000 * bucket_random(body_size_buckets) * bucket_random(body_size_buckets) * bucket_random(body_size_buckets)
-                const strength = bucket_random(dwarf_strength_buckets)
-                const effweight = size/100 + weapon_weight * 100
-                const velocity = size * strength * attack.velocity / (1000 * effweight)
-                const momentum = velocity * weapon_weight + 1
-                const area = Math.min(attack.area, size*0.036)
-                let won = false
-                if(attack.edged) {
-                    if (momentum >= ((armor_mat.shearYield / weapon_mat.shearYield) + ((area + 1) * (armor_mat.shearFracture / weapon_mat.shearFracture)) * ((10 + 2*Qa) / (Qw * weapon_mat.maxEdge / 10000)))) {
-                        successes++
-                        won = true
-                    }
-                }
-                if(!won && (2*weapon.size * weapon_mat.impactYield / 1000000 >= area * armor_mat.solidDensity / 1000)) {
-                    if (momentum >= (((2 * armor_mat.impactFracture)/1000000 - armor_mat.impactYield/1000000) * (2 + 0.4 * Qa) * area)) {
-                        successes++
-                    }
-                }
-                trials++
-            }
-        }
-    }
-    return Math.round(10 * successes/trials)
-}*/
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_size(u: usize);
+}
 
 const QUALITY_WEAPON_MULTS: [f64; 6] = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
@@ -104,41 +78,112 @@ pub fn attack_score(
     let weapon: Weapon = weapon_js.into_serde().unwrap();
     let mut trials = 0;
     let mut rng = thread_rng();
-    let successes = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6].iter().map(|&a| {
-        let Qa = QUALITY_ARMOR_MULTS[a as usize];
-        [0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5]
-            .iter()
-            .map(|&w| {
-                let Qw = QUALITY_WEAPON_MULTS[w as usize];
-                let size = 60000.0
-                    * (0..3).fold(1.0, |acc, _| {
-                        acc * bucket_random(&BODY_SIZE_BUCKETS, &mut rng)
-                    });
-                let enemy_size = 60000.0
-                * (0..3).fold(1.0, |acc, _| {
-                    acc * bucket_random(&BODY_SIZE_BUCKETS, &mut rng)
-                });
-                let strength = bucket_random(&DWARF_STRENGTH_BUCKETS, &mut rng) as f64;
-                let momentum = size * strength * attack.velocity / (10.0 * ((10000.0 + size) / weapon_weight));
-                [0.036, 0.0027].iter().filter(|&&contact_area| { // breastplate, helmet
-                    let area = attack.area.min(enemy_size * contact_area);
-                    trials += 1;
-                    (attack.edged
-                        && momentum
-                            >= ((armor_mat.shearYield / weapon_mat.shearYield)
-                                + ((area + 1.0)
-                                    * (armor_mat.shearFracture / weapon_mat.shearFracture))
-                                    * ((10.0 + 2.0 * Qa) / (Qw * weapon_mat.maxEdge / 10000.0))))
-                        || ((2.0 * weapon.size * weapon_mat.impactYield / 1000.0
-                            >= area * armor_mat.solidDensity)
-                            && (momentum
-                                >= (((2.0 * armor_mat.impactFracture) / 1000000.0
-                                    - armor_mat.impactYield / 1000000.0)
-                                    * (2.0 + 0.4 * Qa)
-                                    * area)))    
-                }).count()
-            })
-            .sum::<usize>()
-    }).sum::<usize>();
+    let successes = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+        .iter()
+        .map(|&a| {
+            let Qa = QUALITY_ARMOR_MULTS[a as usize];
+            [0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+                .iter()
+                .map(|&w| {
+                    let Qw = QUALITY_WEAPON_MULTS[w as usize];
+                    let size = 60000.0
+                        * (0..3).fold(1.0, |acc, _| {
+                            acc * bucket_random(&BODY_SIZE_BUCKETS, &mut rng)
+                        });
+                    let enemy_size = 60000.0
+                        * (0..3).fold(1.0, |acc, _| {
+                            acc * bucket_random(&BODY_SIZE_BUCKETS, &mut rng)
+                        });
+                    let strength = bucket_random(&DWARF_STRENGTH_BUCKETS, &mut rng) as f64;
+                    let momentum = size * strength * attack.velocity
+                        / (10.0 * ((10000.0 + size) / weapon_weight));
+                    [0.036, 0.0027]
+                        .iter()
+                        .filter(|&&contact_area| {
+                            // breastplate, helmet
+                            let area = attack.area.min(enemy_size * contact_area);
+                            trials += 1;
+                            (attack.edged
+                                && momentum
+                                    >= ((armor_mat.shearYield / weapon_mat.shearYield)
+                                        + ((area + 1.0)
+                                            * (armor_mat.shearFracture
+                                                / weapon_mat.shearFracture))
+                                            * ((10.0 + 2.0 * Qa)
+                                                / (Qw * weapon_mat.maxEdge / 10000.0))))
+                                || ((2.0 * weapon.size * weapon_mat.impactYield / 1000.0
+                                    >= area * armor_mat.solidDensity)
+                                    && (momentum
+                                        >= (((2.0 * armor_mat.impactFracture) / 1000000.0
+                                            - armor_mat.impactYield / 1000000.0)
+                                            * (2.0 + 0.4 * Qa)
+                                            * area)))
+                        })
+                        .count()
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>();
     (100.0 * successes as f64 / trials as f64).round() / 10.0
+}
+
+struct Adjacents {
+    index: usize,
+    width: usize,
+    size: usize,
+    i: usize,
+}
+
+impl Iterator for Adjacents {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.i += 1;
+            match self.i {
+                1 => {
+                    if self.index % self.width != 0 {
+                        return Some(self.index - 1);
+                    }
+                }
+                2 => {
+                    if self.index % self.width != (self.width - 1) {
+                        return Some(self.index + 1);
+                    }
+                }
+                3 => {
+                    if self.index >= self.width {
+                        return Some(self.index - self.width);
+                    }
+                }
+                4 => {
+                    if self.index + self.width < self.size {
+                        return Some(self.index + self.width);
+                    }
+                }
+                _ => return None,
+            }
+        }
+    }
+}
+
+fn adjacent_tiles(index: usize, width: usize, size: usize) -> Adjacents {
+    Adjacents {
+        index,
+        width,
+        size,
+        i: 0,
+    }
+}
+
+#[wasm_bindgen]
+pub fn fdm(arr: &[f64], width: usize) -> Box<[f64]> {
+    arr.iter()
+        .enumerate()
+        .map(|(i, f)| {
+            let (num_adj, adjacent_gas) = adjacent_tiles(i, width, arr.len())
+                .fold((0, 0.0), |acc, j| { (acc.0 + 1, acc.1 + arr[j] / 5.0) });
+            (f * (5-num_adj) as f64) / 5.0 + adjacent_gas
+        })
+        .collect::<Vec<f64>>()
+        .into_boxed_slice()
 }
